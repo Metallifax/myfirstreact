cec616b0c1ce83d3ce0b2c3bde5c026911495ed7
Matt Forgione <mathew_forgione@msn.com>
2020-08-10 13:56:44 -0300

    added a random sentence functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const element = <p>This is a sentence!</p>;
+  const sentences = [
+    "Hello world!",
+    "To boldly go where no man has gone before...",
+    "I'm a teapot short and stout",
+    "Oh jeez!",
+    "Heck me!",
+    "Oh heck!",
+    "Jesus loves me yes he does"
+  ];
+  const randomSentence = () => {
+    return sentences[Math.floor(Math.random() * sentences.length)];
+  };
+
+  const myId = "test";
+  const element2 = <h1 id={myId}>Hello, world!</h1>;
+
+  function ActionLink(){
+    function handleClick(e) {
+      e.preventDefault();
+      console.log(randomSentence());
+    }
+    return (
+      <button onClick={handleClick}>
+        Click me
+      </button>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {element2}
+      {element}
+      {randomSentence()}
+      <div>
+        <ActionLink />
+      </div>
     </div>
   );
 }
